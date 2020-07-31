32b11df9b0104d83ef0d40b39b5d874e520a16d1
tanyay532 <tanyay532@gmail.com>
2020-07-31 19:34:55 +0530

    Updates

---
+++ precache-manifest.e4cb2737726728b99787f2baaf296930.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f964704523a6fdc47bf402ad9d8a930b",
+    "url": "/Reactjs_Project/index.html"
+  },
+  {
+    "revision": "7f11928a5aa429e7c991",
+    "url": "/Reactjs_Project/static/css/main.43f34a28.chunk.css"
+  },
+  {
+    "revision": "44a997fbbf9cb4c0af10",
+    "url": "/Reactjs_Project/static/js/2.bfdbfba2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Reactjs_Project/static/js/2.bfdbfba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f11928a5aa429e7c991",
+    "url": "/Reactjs_Project/static/js/main.95d5ea40.chunk.js"
+  },
+  {
+    "revision": "f7f1bd51fa821a4ac504",
+    "url": "/Reactjs_Project/static/js/runtime-main.23c82838.js"
+  }
+]);
